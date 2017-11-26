28f08fedb4a3cb9e960062bfac2a86a42f824084
Matti Panula <mattijaakkopanu@gmail.com>
2017-11-26 21:50:37 +0200

    clear-button works

--- Laskukone/app/src/main/java/fi/tut/matti/laskukone/MainActivity.java
+++ Laskukone/app/src/main/java/fi/tut/matti/laskukone/MainActivity.java
@@ -9,6 +9,13 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/** code has a lot of indexes (magic numbers). I know this is bad practice
+ * but just wanted to avoid tedious duplicate code and to learn parent/sibling handling.
+ * should have probably used a grid for the calculators and have named columns etc.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,8 +27,36 @@ public class MainActivity extends AppCompatActivity {
         addLaskeButtonHandler((Button)findViewById(R.id.miinusbutton), "-");
         addLaskeButtonHandler((Button)findViewById(R.id.kertobutton), "x");
         addLaskeButtonHandler((Button)findViewById(R.id.jakobutton), "/");
+
+        final Button tyhjennaBtn = findViewById(R.id.tyhjenna);
+        final List<TextView> clearableViews = getViewsToClear();
+        tyhjennaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(TextView clearableView : clearableViews){
+                    clearableView.setText("0");
+                }
+            }
+        });
     }
 
+    /** find views that should be set to 0 with the button */
+    private List<TextView> getViewsToClear(){
+        List<TextView> clearableViews = new ArrayList<>();
+        LinearLayout mainLayout = findViewById(R.id.mainlayout);
+        for(int i = 0; i < 4; i++){ // magic number 4: +,-,x,/ <- exlude clear and log buttons (=idx 4)
+            if(mainLayout.getChildAt(i) instanceof  LinearLayout){
+                LinearLayout calcRow = (LinearLayout)mainLayout.getChildAt(i);
+                // magic numbers 0,2,4 <- 2 input fields, and the result field
+                clearableViews.add((TextView) calcRow.getChildAt(0));
+                clearableViews.add((TextView) calcRow.getChildAt(2));
+                clearableViews.add((TextView) calcRow.getChildAt(4));
+            }
+        }
+        return clearableViews;
+    }
+
+    /** find related inputs and outputs by sibling indexes to reduce amount of code */
     private void addLaskeButtonHandler(Button btn, final String operation){
         final LinearLayout buttonParentLayout = (LinearLayout) btn.getParent();
         final EditText ekaText = (EditText) buttonParentLayout.getChildAt(0);
@@ -38,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /** stupid calculator method */
     private double laske(double eka, double toka, String operation){
         if(operation.equals("+")){
             return eka+toka;
