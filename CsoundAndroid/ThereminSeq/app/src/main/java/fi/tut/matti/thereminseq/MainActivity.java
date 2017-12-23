2c6ec8566953322c0c1da6171cad4051c4e91faf
Matti Panula <mattijaakkopanu@gmail.com>
2017-12-23 13:51:35 +0200

    makes noise

--- CsoundAndroid/ThereminSeq/app/src/main/java/fi/tut/matti/thereminseq/MainActivity.java
+++ CsoundAndroid/ThereminSeq/app/src/main/java/fi/tut/matti/thereminseq/MainActivity.java
@@ -7,25 +7,102 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+import com.csounds.CsoundObj;
+import com.csounds.CsoundObjListener;
+import com.csounds.bindings.motion.CsoundMotion;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends Activity implements
+        CsoundObjListener {
     private final String TAG = MainActivity.class.getName();
+    protected CsoundObj csoundObj = new CsoundObj(true,true);
+    ToggleButton startStopButton = null;
 
-    private Button btn;
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        csoundObj.setMessageLoggingEnabled(true);
+
+        startStopButton = findViewById(R.id.mute);
+        startStopButton
+                .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                    public void onCheckedChanged(CompoundButton buttonView,
+                                                 boolean isChecked) {
+                        if (isChecked) {
+                            String csd = getResourceFileAsString(R.raw.hardware_test);
+                            File f = createTempFile(csd);
+
+                            CsoundMotion csoundMotion = new CsoundMotion(csoundObj);
+                            csoundMotion.enableAccelerometer( MainActivity.this);
+
+                            csoundObj.startCsound(f);
+                        } else {
+                            csoundObj.stop();
+                        }
+
+                    }
+                });
 
         Toast.makeText(this, checkAudioCapabilities(), Toast.LENGTH_SHORT).show();
 
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        csoundObj.stop();
+
+    }
+
+    protected String getResourceFileAsString(int resId) {
+        StringBuilder str = new StringBuilder();
+
+        InputStream is = getResources().openRawResource(resId);
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        String line;
+
+        try {
+            while ((line = r.readLine()) != null) {
+                str.append(line).append("\n");
+            }
+        } catch (IOException ios) {
+
+        }
+
+        return str.toString();
+    }
+
+    protected File createTempFile(String csd) {
+        File f = null;
+
+        try {
+            f = File.createTempFile("temp", ".csd", this.getCacheDir());
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(csd.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return f;
     }
 
     private String checkAudioCapabilities(){
@@ -44,5 +121,15 @@ public class MainActivity extends Activity {
             text = "No Android Compatibility Definition Document (CDD) compatible "
                     + "low latency capabilities reported by device.\n";
         return text;
+    }
+
+    @Override
+    public void csoundObjStarted(CsoundObj csoundObj) {
+
+    }
+
+    @Override
+    public void csoundObjCompleted(CsoundObj csoundObj) {
+
     }
 }
